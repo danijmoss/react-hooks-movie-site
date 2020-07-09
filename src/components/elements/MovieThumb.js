c66540a3c218d262be481e297fbb4f18ccdd5044
Danielle "Dani" Moss <dani.j.moss@gmail.com>
2020-07-09 19:35:07 -0400

    added header, header image, movie grid, load more button

--- src/components/elements/MovieThumb.js
+++ src/components/elements/MovieThumb.js
@@ -1,5 +1,15 @@
 import React from "react";
 
-const MovieThumb = () => <div>MovieThumb</div>;
+import { StyledMovieThumb } from "../styles/StyledMovieThumb";
+
+const MovieThumb = ({ image, movieId, clickable }) => (
+  <StyledMovieThumb>
+    {clickable ? (
+      <img className="clickable" src={image} alt="movieThumb" />
+    ) : (
+      <img src={image} alt="movieThumb" />
+    )}
+  </StyledMovieThumb>
+);
 
 export default MovieThumb;
--- src/components/hooks/useHomeFetch.js
+++ src/components/hooks/useHomeFetch.js
@@ -10,15 +10,20 @@ export const useHomeFetch = () => {
     setError(false);
     setLoading(true);
 
+    const isLoadMore = endpoint.search("page");
+
     try {
       const result = await (await fetch(endpoint)).json();
 
       setState((prev) => ({
         ...prev,
-        movies: [...result.results],
+        movies:
+          isLoadMore !== -1
+            ? [...prev.movies, ...result.results]
+            : [...result.results],
         heroImage: prev.heroImage || result.results[0],
         currentPage: result.page,
-        totalPage: result.total_pages,
+        totalPages: result.total_pages,
       }));
     } catch (error) {
       setError(true);
