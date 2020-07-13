9ac116f577a54d5b1bb1db73ec10611b1cc6093c
Danielle "Dani" Moss <dani.j.moss@gmail.com>
2020-07-13 18:41:18 -0400

    created build version and removed node modules

---
+++ build/precache-manifest.855c9c1322421b886c71c47118857555.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e79dcf666fe169b228d684671ea7217e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5067ad9719dee8ef158",
+    "url": "/static/js/2.40ae298f.chunk.js"
+  },
+  {
+    "revision": "1003775914dfca7132b2",
+    "url": "/static/js/main.51c837c1.chunk.js"
+  },
+  {
+    "revision": "7380a67bdde2a68b9cba",
+    "url": "/static/js/runtime~main.21d1648e.js"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "08494abf4210d1ea8b163791db70773c",
+    "url": "/static/media/reactMovie_logo.08494abf.png"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);
